4c0e1feb3de22063164b8466c9e86d46d8f0f001
OlegPrusachenko <92158576+OlegPrusachenko@users.noreply.github.com>
2022-10-07 16:20:01 +0200

    Create FirstFile.js
    HomeWork 30 sept

---
+++ FirstFile.js
@@ -0,0 +1,32 @@
+// All operators mentioned in the presentation
+
+let a = 1;
+let b = 3;
+
+//-----------------------------------------------
+
+if (a >= 2){
+  console.log("A is more or equal two") 
+} else {
+  console.log("A is less then two")
+}
+
+//-----------------------------------------------
+
+a += 5;
+b -= 1;
+if (a < b){
+console.log(true);
+} else {
+rconsole.log(false);
+};
+
+//-----------------------------------------------
+
+let a = 3;
+let b = 11;
+
+console.log(b %= a);
+console.log(b /= a);
+console.log(b *= a);
+console.log(b **= a);
